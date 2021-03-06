1b4cd8f7fb0e7391eddc889ce3f1b742480b0044
threadexio <panos.z@outlook.com>
2021-03-06 18:03:15 +0200

    Added test, made small progress

--- src/main.py
+++ src/main.py
@@ -1,12 +1,32 @@
+from selenium import webdriver
+from time import sleep
 import selenium
 
 checkbox_class = "recaptcha-checkbox-border"
 
-def solve_captcha(driver, iframe) -> bool:
+def solve_captcha(driver :webdriver.Firefox, iframe) -> bool:
 	# Switch current context
 	driver.switch_to.frame(iframe)
 
 	# Get the checkbox
-	checkbox = driver.find_elements_by_class_name(checkbox)
+	checkbox = driver.find_elements_by_class_name(checkbox_class)[0]
 
-	print(checkbox)
+	# Click the checkbox
+	checkbox.click()
+
+	sleep(1)
+
+	driver.switch_to.default_content()
+
+	sleep(1)
+
+	iframes = driver.find_elements_by_tag_name("iframe")
+
+	iframes = driver.find_elements_by_tag_name("iframe")
+	for x in iframes:
+		if x.get_attribute("title") == "recaptcha challenge":
+			driver.switch_to.frame(x)
+
+	sleep(1)
+
+	driver.find_element_by_id("recaptcha-audio-button").click()
